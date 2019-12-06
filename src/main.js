e8516249931fff45d9cdaf363d1aedbb478b3e3c
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-06 15:24:05 -0800

    add more divs to host name of strain, add matching jQuery and api queries

--- src/main.js
+++ src/main.js
@@ -19,9 +19,39 @@ $(document).ready(function() {
 
     function getElements(detail) {
       console.log(detail);
-      $('.info').text(`Detail: ${detail[0].desc}`);
-      $('.type').text(`Type: ${detail[0].race}`);
-      
+
+// name
+      $('.name').text(`Strain Name: ${detail[0].name}`);
+      $('.name1').text(`Strain Name: ${detail[1].name}`);
+      $('.name2').text(`Strain Name: ${detail[2].name}`);
+      $('.name3').text(`Strain Name: ${detail[3].name}`);
+      $('.name4').text(`Strain Name: ${detail[4].name}`);
+      $('.name5').text(`Strain Name: ${detail[5].name}`);
+      $('.name6').text(`Strain Name: ${detail[6].name}`);
+
+
+
+// descriptions
+      $('.info').text(`Description: ${detail[0].desc}`);
+      $('.info1').text(`Description: ${detail[1].desc}`);
+      $('.info2').text(`Description: ${detail[2].desc}`);
+      $('.info3').text(`Description: ${detail[3].desc}`);
+      $('.info4').text(`Description: ${detail[4].desc}`);
+      $('.info5').text(`Description: ${detail[5].desc}`);
+      $('.info6').text(`Description: ${detail[6].desc}`);
+// types
+      $('.type').text(`Type:  ${detail[0].race}`);
+      $('.type1').text(`Type:  ${detail[1].race}`);
+      $('.type2').text(`Type:  ${detail[2].race}`);
+      $('.type3').text(`Type:  ${detail[3].race}`);
+      $('.type4').text(`Type:  ${detail[4].race}`);
+      $('.type5').text(`Type:  ${detail[5].race}`);
+      $('.type6').text(`Type:  ${detail[6].race}`);
+
     }
   });
 });
+
+
+// $('.info').text(`Detail: ${detail[0].desc}, ${detail[1].desc}, ${detail[2].desc}, ${detail[3].desc}, ${detail[4].desc}, ${detail[5].desc}, ${detail[6].desc}, ${detail[7].desc}`);
+// $('.type').text(`Type: ${detail[0].race}, ${detail[1].race}, ${detail[2].race}, ${detail[3].race}, ${detail[4].race}, ${detail[5].race}, ${detail[6].race}, ${detail[7].race}`);
