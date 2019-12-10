1372ec02b65f559677f4167739bf658536d22a1d
Student <example@example.com>
2019-12-09 16:25:19 -0800

    add more styles and changed api back to strain name only

--- src/randomuser.js
+++ src/randomuser.js
@@ -1,7 +1,7 @@
 export class Strain {
   async getProfile(nameOfStrain) {
     try {
-      let response = await fetch(`https://strainapi.evanbusse.com/Pi2XNoO/strains/data/effects/${nameOfStrain}`);
+      let response = await fetch(`https://strainapi.evanbusse.com/Pi2XNoO/strains/search/name/${nameOfStrain}`);
       // let response = await fetch(`strainapi.evanbusse.com/API_KEY/strains/data/effects/STRAIN_ID`);
       console.log(response);
       let jsonifiedResponse = await response.json();
