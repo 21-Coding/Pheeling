076f59790a529d221a0b262142c95dc1520d76c9
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-23 16:54:20 -0800

    add remvoval of wireframing for indentity stats random user code

--- src/randomuser.js
+++ src/randomuser.js
@@ -11,15 +11,4 @@ export class Strain {
     }
   }
 
-  // async getProfile(nameOfStrain) {
-  //   try {
-  //     let response = await fetch(`https://strainapi.evanbusse.com/Pi2XNoO/strains/search/name/${nameOfStrain}`);
-  //     // let response = await fetch(`https://strainapi.evanbusse.com/Pi2XNoO/strains/search/all`);
-  //     console.log(response);
-  //     let jsonifiedResponse = await response.json();
-  //     return jsonifiedResponse;
-  //   } catch(error) {
-  //     console.error("There was an error handling your request: " + error.message);
-  //   }
-  // }
 }
