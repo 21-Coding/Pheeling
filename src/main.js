190177c32c94a355729e152d97f4d08c1b687cb8
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-23 16:53:30 -0800

    add functionality to for loop and if else statement, results show but will not append

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#detail-button').click(function() {
+    $('.hideInput').hide();
+    $('.scroll').fadeIn();
 
     const strainName = $("#nameOfStrain").val();
 
@@ -18,39 +20,74 @@ $(document).ready(function() {
     })();
 
     function getElements(detail) {
-      console.log(detail);
+      let each = detail.length;
+      if (each === 1) {
+        $('.scroll').show();
+        $('#queryCount').text(each);
+      } else {
+        $('.noStrains').show()
+        $('.scroll').hide();
+        $('#queryCount').text(each);
+      }
 
-// na$('.name').text(`Strain Name: ${detail[0].name}`);
-      $('.name').text(`Strain Name: ${detail[0].name}`);
-      $('.name1').text(`Strain Name: ${detail[1].name}`);
-      $('.name2').text(`Strain Name: ${detail[2].name}`);
-      $('.name3').text(`Strain Name: ${detail[3].name}`);
-      $('.name4').text(`Strain Name: ${detail[4].name}`);
-      $('.name5').text(`Strain Name: ${detail[5].name}`);
-      $('.name6').text(`Strain Name: ${detail[6].name}`);
+      for (let i = 0; i < each; i ++) {
+
+        let name = detail[i].name;
+        let desc = detail[i].desc;
+        let race = detail[i].race;
+        if (each === true) {
+          $('.scroll').append(`
+          <div>
+            <ul>
+              <li>${name}</li>
+              <li>${desc}</li>
+              <li>${race}</li>
+            </ul>
+          </div>
+              `)
+
+        } else {
+         console.log(detail);
+        }
 
 
 
-// descriptions
-      $('.info').text(`Description: ${detail[0].desc}`);
-      $('.info1').text(`Description: ${detail[1].desc}`);
-      $('.info2').text(`Description: ${detail[2].desc}`);
-      $('.info3').text(`Description: ${detail[3].desc}`);
-      $('.info4').text(`Description: ${detail[4].desc}`);
-      $('.info5').text(`Description: ${detail[5].desc}`);
-      $('.info6').text(`Description: ${detail[6].desc}`);
-// types
-      $('.type').text(`Type:  ${detail[0].race}`);
-      $('.type1').text(`Type:  ${detail[1].race}`);
-      $('.type2').text(`Type:  ${detail[2].race}`);
-      $('.type3').text(`Type:  ${detail[3].race}`);
-      $('.type4').text(`Type:  ${detail[4].race}`);
-      $('.type5').text(`Type:  ${detail[5].race}`);
-      $('.type6').text(`Type:  ${detail[6].race}`);
 
+      }
     }
   });
 });
+
+
+
+// // na$('.name').text(`Strain Name: ${detail[0].name}`);
+//       $('.name').text(`Strain Name: ${detail[0].name}`);
+//       $('.name1').text(`Strain Name: ${detail[1].name}`);
+//       $('.name2').text(`Strain Name: ${detail[2].name}`);
+//       $('.name3').text(`Strain Name: ${detail[3].name}`);
+//       $('.name4').text(`Strain Name: ${detail[4].name}`);
+//       $('.name5').text(`Strain Name: ${detail[5].name}`);
+//       $('.name6').text(`Strain Name: ${detail[6].name}`);
+//
+//
+//
+// // descriptions
+//       $('.info').text(`Description: ${detail[0].desc}`);
+//       $('.info1').text(`Description: ${detail[1].desc}`);
+//       $('.info2').text(`Description: ${detail[2].desc}`);
+//       $('.info3').text(`Description: ${detail[3].desc}`);
+//       $('.info4').text(`Description: ${detail[4].desc}`);
+//       $('.info5').text(`Description: ${detail[5].desc}`);
+//       $('.info6').text(`Description: ${detail[6].desc}`);
+// // types
+//       $('.type').text(`Type:  ${detail[0].race}`);
+//       $('.type1').text(`Type:  ${detail[1].race}`);
+//       $('.type2').text(`Type:  ${detail[2].race}`);
+//       $('.type3').text(`Type:  ${detail[3].race}`);
+//       $('.type4').text(`Type:  ${detail[4].race}`);
+//       $('.type5').text(`Type:  ${detail[5].race}`);
+//       $('.type6').text(`Type:  ${detail[6].race}`);
+
 
 
 // $('.info').text(`Detail: ${detail[0].desc}, ${detail[1].desc}, ${detail[2].desc}, ${detail[3].desc}, ${detail[4].desc}, ${detail[5].desc}, ${detail[6].desc}, ${detail[7].desc}`);
